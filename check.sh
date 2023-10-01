ae8374745b0897a12f5ae7567d8fe1691def7311
JChristensen <jack.christensen@outlook.com>
2023-10-01 15:49:47 -0400

    Add check.sh script.

---
+++ check.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Check trace logs for various errors.
+# Run this script from the directory that contains the log files.
+# J.Christensen 01Oct2023
+
+echo -n "Count of script runs in yesterday's log: "
+nrun=$(egrep -i "start:" trace.log.1 | wc -l)
+echo "$nrun"
+
+echo -e "\nChecking for errors..."
+egrep -i "error" trace*
+status=$?
+if (( $status )); then
+    if (( $status == 1 )); then
+        echo "...none found."
+    else
+        echo "...grep error, status $status."
+    fi
+fi
+
+echo -e "\nChecking for missed responses from target server..."
+egrep -i "\*\ +8\." trace*
+status=$?
+if (( $status )); then
+    if (( $status == 1 )); then
+        echo "...none found."
+    else
+        echo "...grep error, status $status."
+    fi
+fi
